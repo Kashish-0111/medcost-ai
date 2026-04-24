816ca87f00b2226cea49b08a433d3f5eb8fb81ac
Kashish-0111 <kashishdhankar57@gmail.com>
2026-04-24 20:24:46 +0530

    setup done

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors({ origin: "*" }))
+app.use(express.json())
+
+connectDB()
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'MedCost AI Backend Running 🚀' })
+})
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`)
+})
